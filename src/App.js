054e37874b7c1f75166ba5a597630d38a6c52a17
shdwu <shdwu@shdwus-MacBook-Pro.local>
2021-04-19 21:13:05 +0800

    123

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import { Steps, Button, message } from 'antd';
+import { Steps, Button } from 'antd';
 import Step1 from './Step1';
 import Step2 from './Step2';
+import Step3 from './Step3';
 
 const { Step } = Steps;
 
@@ -11,6 +12,7 @@ function App() {
   const [current, setCurrent] = React.useState(0);
   const [step1Data, setStep1Data] = React.useState([]);
   const [step2Data, setStep2Data] = React.useState([]);
+  const [step3Data, setStep3Data] = React.useState([]);
   const steps = [
     {
       title: '第一步: 传入源文件',
@@ -22,7 +24,7 @@ function App() {
     },
     {
       title: '第三步: 生成结果',
-      content: '生成结果',
+      content: <Step3 data={step3Data} setData={setStep3Data} />
     },
   ];
 
@@ -33,6 +35,18 @@ function App() {
   const prev = () => {
     setCurrent(current - 1);
   };
+
+  const mergeData = () => {
+    const mergeList = [];
+    step1Data.forEach(it => {
+      const mergeData = {...it};
+      const finded = step2Data.find(it.name)
+      mergeData.phone = finded.phone
+      mergeData.zhifubao = finded.zhifubao
+      mergeList.push(mergeData);
+    })
+    setStep3Data(mergeList);
+  }
 
   return (
     <>
@@ -54,7 +68,7 @@ function App() {
           </Button>
         )}
         {current === steps.length - 1 && (
-          <Button type="primary" onClick={() => message.success('Processing complete!')}>
+          <Button type="primary" onClick={() => mergeData()}>
             完成
           </Button>
         )}
---
+++ src/Step3Table.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Table, Space, Button } from 'antd';
+import { CloseOutlined } from '@ant-design/icons';
+
+
+
+class Step3Table extends React.Component {
+
+  columns = [
+    {
+      title: '昵称',
+      dataIndex: 'nick',
+      key: 'nick',
+    },
+    {
+      title: 'uuid',
+      dataIndex: 'uuid',
+      key: 'uuid',
+    },
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '语音收益',
+      dataIndex: 'income',
+      key: 'income',
+    },
+    {
+      title: '星挑战+工会奖励',
+      dataIndex: 'reward',
+      key: 'reward',
+    },
+    {
+      title: '实发',
+      dataIndex: 'factIncome',
+      key: 'factIncome',
+    },
+    {
+      title: '注册手机号',
+      dataIndex: 'phone',
+      key: 'phone',
+    },
+    {
+      title: '支付宝号',
+      dataIndex: 'zhifubao',
+      key: 'zhifubao',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      // eslint-disable-next-line react/display-name
+      render: (_text, _record, index) => (
+        <Space size="middle">
+          <Button type="primary" shape="circle" danger icon={<CloseOutlined />} onClick={() => this.props.removeItem(index)} />
+        </Space>
+      ),
+    },
+  ];
+
+  render() {
+    return <Table columns={this.columns} dataSource={this.props.data} />
+  }
+}
+
+Step3Table.propTypes = {
+  data: PropTypes.array.isRequired,
+  removeItem: PropTypes.func.isRequired
+}
+
+export default Step3Table;
+
